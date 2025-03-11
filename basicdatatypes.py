e1610fd43fba14e1b72f0dbfad814bda4d707fa1
suhanipm <suhanimadammanavar170503@gmail.com>
2025-03-11 23:38:28 +0530

    bdatatypes

---
+++ basicdatatypes.py
@@ -0,0 +1,17 @@
+a = 10
+print(a, type(a))
+
+b = 12.33
+print(b, type(b))
+
+c='k'
+print(c, type(c))
+
+d= 'Code'
+print(d, type(d))
+
+e = True
+print(e, type(e))
+
+h = 3 + 5j
+print(h , type(h))
