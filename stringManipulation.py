708cb0d165a49723e0ca4c584abf391e93865f01
suhanipm <suhanimadammanavar170503@gmail.com>
2025-03-12 11:49:21 +0530

    stringsdatatype

---
+++ stringManipulation.py
@@ -0,0 +1,12 @@
+s1 = 'Code with me,'
+print(s1[0]) # 0th index  value from s1 string
+print(s1[1]) # 1th index  value from s1 string
+print(s1[2]) # 2th index  value from s1 string
+print(s1[3])# 3th index  value from s1 string
+print(s1[12])
+
+
+print('Length of s1 is:',len(s1)) #length of string
+
+print(s1[-1]) #fetch last index value
+print(s1[-3]) #fetch last index values
--- stringsdatatype.py
+++ stringsdatatype.py
@@ -1,1 +1,20 @@
 name = 'San'
+print(name)
+
+
+info = '''i 'm' "san" '''
+print(info)
+
+poem = '''Twinkle, twinkle, little star, 
+How I wonder what you are!  
+Up above the world so high,  
+Like a diamond in the sky.  '''    #Multiline strings
+
+
+
+print(poem)
+
+''' these statements are in  comments '''
+
+data = "Co"+"de"
+print(data) #code #string concatenation
